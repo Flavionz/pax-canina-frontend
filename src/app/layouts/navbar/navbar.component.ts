754f4b96767c2f25da31a25a060a95cf4ef15deb
Flavio <[200~flavio.terenzi@stagiairesmns.frflavio.terenzi@stagiairesmns.frgit config --global user.email flavio.terenzi@stagiairesmns.fr>
2025-04-15 16:53:26 +0200

    Corrections navbar et amélioration responsivité
    - Problèmes de layout/espacement résolus
    - Meilleure portabilité mobile
    - Nouveau placement du bouton déconnexion
    - Tests de compatibilité
    - Nettoyage du CSS

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,18 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isMobileView = false;
   isMenuOpen = false;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.setupResponsiveBehavior();
+  }
+
+  private setupResponsiveBehavior() {
+    this.breakpointObserver.observe([
+      Breakpoints.Handset,
+      Breakpoints.TabletPortrait,
+      '(max-width: 768px)'
+    ]).subscribe(result => {
+      this.isMobileView = result.matches;
+
+      if (!this.isMobileView) {
+        this.isMenuOpen = false;
+      }
+    });
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    if (this.isMobileView) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  logout() {
+    console.log('Logout effectuate');
+    this.router.navigate(['/auth/logout']);
+    this.isMenuOpen = false;
   }
 }
